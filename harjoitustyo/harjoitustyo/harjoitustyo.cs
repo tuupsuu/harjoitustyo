13dc1856cdfa97b5996453bf9d421e12ef7afe78
tuupsuu <topias.liljegren@gmail.com>
2021-10-11 15:50:10 +0300

    maali luotu

--- harjoitustyo/harjoitustyo/harjoitustyo.cs
+++ harjoitustyo/harjoitustyo/harjoitustyo.cs
@@ -7,13 +7,15 @@ using System.Collections.Generic;
 
 public class harjoitustyo : PhysicsGame
 {
-    int kenttaNro = 1;
-    PhysicsObject pelaaja;
-    Vector nopeusYlos = new Vector(0, 100);
-    Vector nopeusVasemmalle = new Vector(-100, 0);
-    Vector nopeusAlas = new Vector(0, -100);
-    Vector nopeusOikealle = new Vector(100, 0);
-    int juoksuKerroin = 2;
+    private int kenttaNro = 1;
+    private static double nopeus = 200;
+    private PhysicsObject pelaaja;
+    private PhysicsObject maali;
+    private Vector nopeusYlos = new Vector(0, nopeus);
+    private Vector nopeusVasemmalle = new Vector(-nopeus, 0);
+    private Vector nopeusAlas = new Vector(0, -nopeus);
+    private Vector nopeusOikealle = new Vector(nopeus, 0);
+    // int juoksuKerroin = 2;
     public override void Begin()
     {
         // Kirjoita ohjelmakoodisi tähän
@@ -39,53 +41,75 @@ public class harjoitustyo : PhysicsGame
         if (a == 1)
         {
             Level.Background.CreateGradient(Color.BloodRed, Color.SkyBlue); // TODO: luo taustakuva huoneen lattiaksi
+            LuoMaali(Level.Left + 50, 0);
+            maali.Oscillate(Vector.UnitY, 100, 0.2);
         }
         else if (a == 2)
         {
             Level.Background.CreateGradient(Color.Orange, Color.BrightGreen);
+            LuoMaali(0, Level.Top - 50);
+            maali.Oscillate(Vector.UnitX, 200, 0.4);
         }
         else if (a == 3)
         {
             Level.Background.CreateGradient(Color.White, Color.Black);
+            LuoMaali(Level.Left + 50, Level.Top - 50);
         }
+
+        Level.CreateBorders();
+        Camera.ZoomToLevel();
+    }
+
+    private void LuoMaali(double x, double y)
+    {
+        maali = new PhysicsObject(50, 50, Shape.Rectangle, x, y);
+        maali.Tag = "maali";
+        maali.CanRotate = true;
+        maali.AngularVelocity = 100;
+        Add(maali);
     }
 
     private void LuoPelaaja()
     {
-        Keyboard.Listen(Key.Space, ButtonState.Pressed, Seuraava, "seuraava kenttä");
+        // Keyboard.Listen(Key.Space, ButtonState.Pressed, Seuraava, "seuraava kenttä");
 
         pelaaja = new PhysicsObject(50, 50, Shape.Rectangle); // TODO: luo pelaajalle oma sprite
         pelaaja.Color = Color.DarkBlue;
         Add(pelaaja);
 
         Keyboard.Listen(Key.A, ButtonState.Down, Liiku, "liikuttaa pelaajaa vasemmalle", pelaaja, nopeusVasemmalle);
-        Keyboard.Listen(Key.A, ButtonState.Released, Liiku, "pysäyttää pelaajan liikkeen", pelaaja, Vector.Zero);
+        Keyboard.Listen(Key.A, ButtonState.Released, Liiku, null, pelaaja, Vector.Zero);
 
         Keyboard.Listen(Key.D, ButtonState.Down, Liiku, "liikuttaa pelaajaa oikealle", pelaaja, nopeusOikealle);
-        Keyboard.Listen(Key.D, ButtonState.Released, Liiku, "pysäyttää pelaajan liikkeen", pelaaja, Vector.Zero);
+        Keyboard.Listen(Key.D, ButtonState.Released, Liiku, null, pelaaja, Vector.Zero);
 
         Keyboard.Listen(Key.W, ButtonState.Down, Liiku, "liikuttaa pelaajaa ylös", pelaaja, nopeusYlos);
-        Keyboard.Listen(Key.W, ButtonState.Released, Liiku, "pysäyttää pelaajan liikkeen", pelaaja, Vector.Zero);
+        Keyboard.Listen(Key.W, ButtonState.Released, Liiku, null, pelaaja, Vector.Zero);
 
         Keyboard.Listen(Key.S, ButtonState.Down, Liiku, "liikuttaa pelaajaa alas", pelaaja, nopeusAlas);
-        Keyboard.Listen(Key.S, ButtonState.Released, Liiku, "pysäyttää pelaajan liikkeen", pelaaja, Vector.Zero);
+        Keyboard.Listen(Key.S, ButtonState.Released, Liiku, null, pelaaja, Vector.Zero);
 
-        // Keyboard.Listen(Key.LeftShift, ButtonState.Pressed, Syoksy, "pelaaja juoksee painettaessa alas", pelaaja, juoksuKerroin);
+        AddCollisionHandler(pelaaja, Maalissa);
+
+        pelaaja.MaxVelocity = 100;
+
+        Keyboard.Listen(Key.F1, ButtonState.Pressed, ShowControlHelp, "Näytä ohjeet");
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
     }
 
+    private void Maalissa(PhysicsObject osuvaKohde, PhysicsObject osuttuKohde)
+    {
+        if (osuttuKohde.Tag.ToString() == "maali")
+        {
+            Seuraava();
+        }
+    }
+
     private void Liiku(PhysicsObject pelaaja, Vector nopeus)
     {
-        pelaaja.Velocity = nopeus;
-        if (Keyboard.IsShiftDown()) pelaaja.Velocity = pelaaja.Velocity * 2;
+        pelaaja.Move(nopeus);
     }
-    /*
-    private void Syoksy(PhysicsObject physics, int kerroin)
-    {
-        if (Keyboard.Listen(Key.W, ButtonState.Down))
-        pelaaja.Hit()
-    } */
 
 
     private void Seuraava()
