97067e57ec43929e22868f6932a574a730f84143
tuupsuu <topias.liljegren@gmail.com>
2021-10-20 18:21:16 +0300

    menu luonti
    menut luotu, asetukset puuttuu, luoti menee neljään suuntaan

--- harjoitustyo/harjoitustyo/harjoitustyo.cs
+++ harjoitustyo/harjoitustyo/harjoitustyo.cs
@@ -9,6 +9,10 @@ public class harjoitustyo : PhysicsGame
 {
     private int kenttaNro = 1;
     private static double nopeus = 100;
+    private string[] alkuValikko = { "Aloita", "Asetukset", "Lopeta peli" };
+    private string[] pauseMenu = { "Jatka", "Asetukset", "Palaa aloitusvalikkoon" };
+    private string[] kenttaLapaistyMenu = { "Seuraava kenttä", "Palaa aloitusvalikkoon" };
+    private string[] peliLapiMenu = { "Aloita alusta", "Lopeta peli" };
     private PhysicsObject pelaaja, maali;
     private AssaultRifle pelaajanAse;
     private Vector nopeusYlos = new Vector(0, nopeus);
@@ -17,8 +21,14 @@ public class harjoitustyo : PhysicsGame
     private Vector nopeusOikealle = new Vector(nopeus, 0);
     public override void Begin()
     {
-        // Kirjoita ohjelmakoodisi tähän
-        SeuraavaKentta();
+        ClearAll();
+        kenttaNro = 1;
+        MultiSelectWindow alkuValikkoV = new MultiSelectWindow("Dungeon Rush", alkuValikko);
+        alkuValikkoV.DefaultCancel = -1;
+        Add(alkuValikkoV);
+        alkuValikkoV.AddItemHandler(0, SeuraavaKentta); // TODO: luo asetukset valikko ja taustakuva alkuvalikolle
+        // alkuValikkoV.AddItemHandler(1, AsetusMenu);
+        alkuValikkoV.AddItemHandler(2, Exit);
     }
 
 
@@ -34,19 +44,24 @@ public class harjoitustyo : PhysicsGame
 
     }
 
+    private void AsetusMenu()
+    {
+        
+    }
+
     private void LuoKentta(int a)
     {
         if (a == 1)
         {
             Level.Background.CreateGradient(Color.BloodRed, Color.SkyBlue); // TODO: luo taustakuva huoneen lattiaksi
             LuoMaali(Level.Left + 50, 0);
-            maali.Oscillate(Vector.UnitY, 100, 0.2);
+            // maali.Oscillate(Vector.UnitY, 100, 0.2);
         }
         else if (a == 2)
         {
             Level.Background.CreateGradient(Color.Orange, Color.BrightGreen);
             LuoMaali(0, Level.Top - 50);
-            maali.Oscillate(Vector.UnitX, 200, 0.4);
+            // maali.Oscillate(Vector.UnitX, 200, 0.4);
         }
         else if (a == 3)
         {
@@ -64,21 +79,23 @@ public class harjoitustyo : PhysicsGame
         maali.Tag = "maali";
         maali.CanRotate = true;
         maali.AngularVelocity = 100;
+        maali.MakeStatic();
         Add(maali);
     }
 
     private void LuoPelaaja()
     {
-        Keyboard.Listen(Key.Space, ButtonState.Pressed, Seuraava, "seuraava kenttä");
+        Keyboard.Listen(Key.Enter, ButtonState.Pressed, Seuraava, "seuraava kenttä");
 
         pelaaja = new PhysicsObject(50, 50, Shape.Rectangle); // TODO: luo pelaajalle oma sprite
         pelaaja.Color = Color.DarkBlue;
         Add(pelaaja);
 
-        pelaajanAse = new AssaultRifle(50, 20); // TODO: luo aseelle uusi skin ja ehkä ääniefekti 
+        pelaajanAse = new AssaultRifle(0, 0); // TODO: luo aseelle uusi skin ja ehkä ääniefekti tai poista eseen sprite ja ammu tulipalloja pelaajasta
         pelaajanAse.Ammo.Value = 1;
         pelaajanAse.FireRate = 10;
         pelaajanAse.ProjectileCollision = AmmusOsui;
+        pelaajanAse.Image = null;
         pelaaja.Add(pelaajanAse);
 
         LuoOhjaimet();
@@ -103,14 +120,13 @@ public class harjoitustyo : PhysicsGame
         Keyboard.Listen(Key.S, ButtonState.Down, Liiku, "liikuttaa pelaajaa alas", nopeusAlas);
         Keyboard.Listen(Key.S, ButtonState.Released, Pysayta, null);
         // yllä pelaajan liikuttaminen, alla pelaajan ampuminen
-        Keyboard.Listen(Key.Left, ButtonState.Pressed, Ammu, "ampuu vasemmalle", pelaajanAse);
-        Keyboard.Listen(Key.Right, ButtonState.Pressed, Ammu, "ampuu oikealle", pelaajanAse);
-        Keyboard.Listen(Key.Up, ButtonState.Pressed, Ammu, "ampuu ylös", pelaajanAse);
-        Keyboard.Listen(Key.Down, ButtonState.Pressed, Ammu, "ampuu alas", pelaajanAse);
+        Keyboard.Listen(Key.Left, ButtonState.Pressed, Ammu, "ampuu vasemmalle", pelaajanAse, nopeusVasemmalle);
+        Keyboard.Listen(Key.Right, ButtonState.Pressed, Ammu, "ampuu oikealle", pelaajanAse, nopeusOikealle);
+        Keyboard.Listen(Key.Up, ButtonState.Pressed, Ammu, "ampuu ylös", pelaajanAse, nopeusYlos);
+        Keyboard.Listen(Key.Down, ButtonState.Pressed, Ammu, "ampuu alas", pelaajanAse, nopeusAlas);
 
         Keyboard.Listen(Key.F1, ButtonState.Pressed, ShowControlHelp, "Näytä ohjeet");
-        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
-        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, PauseValikko, "Avaa valikko");
     }
 
 
@@ -124,7 +140,22 @@ public class harjoitustyo : PhysicsGame
     {
         if (osuttuKohde.Tag.ToString() == "maali")
         {
-            Seuraava();
+            if (kenttaNro == 3)
+            {
+                MultiSelectWindow peliLapi = new MultiSelectWindow("Viimeinen kenttä läpäisty", peliLapiMenu);
+                Add(peliLapi);
+                peliLapi.AddItemHandler(0, Begin);
+                peliLapi.AddItemHandler(1, Exit);
+                peliLapi.DefaultCancel = -1;
+            }
+            else
+            {
+                MultiSelectWindow kenttaLapi = new MultiSelectWindow("Kenttä läpäisty", kenttaLapaistyMenu);
+                Add(kenttaLapi);
+                kenttaLapi.AddItemHandler(0, Seuraava);
+                kenttaLapi.AddItemHandler(1, Begin);
+                kenttaLapi.DefaultCancel = -1;
+            }
         }
     }
 
@@ -138,8 +169,9 @@ public class harjoitustyo : PhysicsGame
         pelaaja.Stop();
     }
 
-    private void Ammu(AssaultRifle ase)
+    private void Ammu(AssaultRifle ase, Vector suunta)
     {
+        pelaajanAse.Angle = suunta.Angle;
         PhysicsObject ammus = ase.Shoot(); // TODO: luo ammukselle uusi skin
         if (ammus != null)
         {
@@ -147,6 +179,17 @@ public class harjoitustyo : PhysicsGame
         }
     }
 
+
+    private void PauseValikko()
+    {
+        IsPaused = true;
+        MultiSelectWindow pauseMenuV = new MultiSelectWindow("Peli pysäytetty", pauseMenu);
+        pauseMenuV.Color = Color.DarkRed; pauseMenuV.SetButtonColor(Color.Black); pauseMenuV.SetButtonTextColor(Color.Red);
+        Add(pauseMenuV);
+        pauseMenuV.AddItemHandler(0, delegate { Remove(pauseMenuV); IsPaused = false;});
+        // pauseMenuV.AddItemHandler(1, AsetusMenu);
+        pauseMenuV.AddItemHandler(2, Begin);
+    }
 
     private void Seuraava()
     {
